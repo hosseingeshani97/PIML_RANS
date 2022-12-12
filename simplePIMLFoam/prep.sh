91a17465d0d6e45a04165343bc341cc7cbef68ec
hosseingeshani97 <57587847+hosseingeshani97@users.noreply.github.com>
2022-12-12 05:57:43 -0500

    Add files via upload

---
+++ simplePIMLFoam/prep.sh
@@ -0,0 +1,8 @@
+export PYTHON_INCLUDE_PATH=/home/hossein/miniconda3/envs/newenv/include/python3.10
+export PYTHON_LIB_PATH=/home/hossein/miniconda3/envs/newenv/lib
+export PYTHON_BIN_PATH=/home/hossein/miniconda3/envs/newenv/bin
+export NUMPY_INCLUDE_PATH=/home/hossein/miniconda3/envs/newenv/lib/python3.10/site-packages/numpy/core/include
+
+export PYTHON_LIB_NAME=-lpython3.10
+export LD_LIBRARY_PATH=$PYTHON_LIB_PATH:$LD_LIBRARY_PATH
+
---
+++ simplePIMLFoam/python_module.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Custom activation (swish)
+
+def CNN_func(a,r):
+    result =  a+r
+    return result
+
+if __name__ == '__main__':
+    print('This is the Python module for test')
